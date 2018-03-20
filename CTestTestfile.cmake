15f8ad1b3a0f618cdeba13b94dd7c18c09185f6b
DimaProskurin <dimapr117@gmail.com>
2018-03-20 15:19:38 +0300

    making cmake

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dmitry/CLionProjects/BattleOfRaces
+# Build directory: /home/dmitry/CLionProjects/BattleOfRaces
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "tests.h"
+
+#ifdef WITH_TEST
+#define INIT_GTEST(argc, argv) \
+::InitGoogleTest(argc, argv);\
+return RUN_ALL_TESTS()
+#else
+#define INIT_GTEST(argc, argv)
+#endif
+
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    INIT_GTEST(&argc, argv);
 }
